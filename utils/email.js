c146dc505d0819d245356f51c49b6d5ac1fb2070
developercodingarrow <digitalcloudinfo@gmail.com>
2024-10-19 16:40:16 +0530

    check live mail

--- utils/email.js
+++ utils/email.js
@@ -2,36 +2,15 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   let transporter = nodemailer.createTransport({
-    host: process.env.BREVO_EMAIL_SMTP_Server,
-    port: process.env.BREVO_EMAIL_PORT,
+    host: "smtp-relay.brevo.com",
+    port: 587,
     auth: {
-      user: process.env.BREVO_LOGIN, // generated ethereal user
-      pass: process.env.BREVO_EMAIL_PASSWORD, // generated ethereal password
+      user: "7e3ce8001@smtp-brevo.com", // generated ethereal user
+      pass: "5mKUWrPRTE8hAtwd", // generated ethereal password
     },
   });
 
   // Differentiate between development and production environments
-  console.log("test email enviroment---", process.env.NODE_ENV);
-  // let transporter = nodemailer.createTransport({
-  //   host:
-  //     process.env.NODE_ENV === "production"
-  //       ? process.env.BREVO_EMAIL_SMTP_Server
-  //       : process.env.EMAIL_HOST,
-  //   port:
-  //     process.env.NODE_ENV === "production"
-  //       ? process.env.BREVO_EMAIL_PORT
-  //       : process.env.EMAIL_PORT,
-  //   auth: {
-  //     user:
-  //       process.env.NODE_ENV === "production"
-  //         ? process.env.BREVO_LOGIN
-  //         : process.env.EMAIL_USER,
-  //     pass:
-  //       process.env.NODE_ENV === "production"
-  //         ? process.env.BREVO_EMAIL_PASSWORD
-  //         : process.env.EMAIL_PASSWORD,
-  //   },
-  // });
 
   const mailOptions = {
     from: '"pinbuzzers 👻" <info@pinbuzzers.com>', // sender address
